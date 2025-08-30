60806172fc465cc8ef2dcf1895ec73ad0d397265
GhosTHaise <ghostrex2@gmail.com>
2025-08-30 21:12:44 +0300

    setup praisonaiagents

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from ollama-ai-agent-mcp!")
+from praisonaiagents import Agent, MCP
 
+search_agent = Agent(
+    instructions="""You help book apartments on Airbnb.""",
+    llm="ollama/llama3",
+    tools=MCP("npx -y @openbnb/mcp-server-airbnb --ignore-robots-txt")
+)
 
-if __name__ == "__main__":
-    main()
+search_agent.start("Search fo Apartments in Antananarivo , Madagascar for 2 nights. 28/04/25 - 30/04/25 for 50 euros per night")
